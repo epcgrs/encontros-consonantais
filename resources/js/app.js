4b7bec41384ecd566e5316e334649888095a956c
Emmanuel <emmanuelf988@gmail.com>
2020-06-30 22:09:18 -0300

    exercício e resultado palavra 1

---
+++ resources/js/app.js
@@ -0,0 +1,53 @@
+require('./bootstrap');
+
+import jQuery from 'jquery';
+window.$ = window.jQuery = jQuery;
+
+import 'popper.js';
+import 'bootstrap';
+
+($ => {
+    $(() => {
+        const word1 = {
+            word: "gráfico",
+            syllables: [
+                'grá',
+                'fi',
+                'co'
+            ],
+            type: "ECP"
+        };
+
+
+
+
+
+        $(document).on('click', '#submitExercise', function (e) {
+            e.preventDefault();
+
+            let word1Input = $('#input-one').val();
+            let word1Type = $('#type-one').val();
+
+            let word1ResultTemplate = `
+            <ul class="list-group">
+                <li class="list-group-item">
+                    <p class="h4 mb-3">Palavra: gráfico</p>
+                    <div class="form-group">
+                        <p class="h5">Sílabas: grá-fi-co</p>
+                    </div>
+                    <div class="form-group">
+                        <p class="h5">Tipo: ECP</p>
+                    </div>
+                    <div class="result">
+                    ${ (word1Input === word1.syllables.join('-') && word1Type === word1.type) ? 'Acertou! &#x1F604;' : 'Errou! &#x1F615;' }
+                    </div>
+                </li>
+            </ul>
+            `;
+
+
+            $('.results-wrapper').html(word1ResultTemplate)
+            console.log(word1ResultTemplate);
+        });
+    });
+})(jQuery);
